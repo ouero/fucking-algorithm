c1c80f2ae774594848252cc4123233c82918edfb
1972236454 <972236454@qq.com>
2020-09-16 21:29:57 +0800

    提交第227题

---
+++ code/src/main/java/_227/TestClass.java
@@ -0,0 +1,37 @@
+package _227;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClass {
+   Solution solution = new Solution();
+
+    @Test
+    public void test01() {
+        int s = solution.calculate("1+1");
+        Assert.assertEquals(2,s);
+    }
+
+    @Test
+    public void test02() {
+        int s = solution.calculate("1+1*7");
+        Assert.assertEquals(8,s);
+    }
+    @Test
+    public void test03() {
+        int s = solution.calculate("5-3*1+2");
+        Assert.assertEquals(4,s);
+    }
+    @Test
+    public void test04() {
+        int s = solution.calculate("-9+6*4/2+1");
+        Assert.assertEquals(4,s);
+    }
+    @Test
+    public void test05() {
+        int s = solution.calculate(" 3/2 ");
+        Assert.assertEquals(1,s);
+    }
+
+}
